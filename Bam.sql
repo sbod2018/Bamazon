7b55d53d363f9a72b31db3b53eee3d25d4fe68dc
Shauna <sboddwell2@gmail.com>
2018-09-24 18:10:05 -0400

    final commit

--- Bam.sql
+++ Bam.sql
@@ -4,10 +4,25 @@ CREATE DATABASE bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products(
-id INT NOT NULL AUTO_INCREMENT,
-item_id VARCHAR(45),
-product_name VARCHAR(45),
-department_name VARCHAR(45),
-price INT default 0,
-PRIMARY KEY (id)
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price DECIMAL (10,2) NULL,
+stock_quanity INT NULL,
+PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ('Taste of The Wild', 'Pet Supplies', 35.99, 275),
+('Taste of The Wild', 'Pet Supplies', 35.99, 275),
+('Skippy', 'Food', 4.00, 455),
+('Raw Bone', 'Pet Supplies', 3.99, 125),
+('Vega One', 'Health and Personal Care', 49.57, 345),
+('Vega Sport Hydrator', 'Health and Personal Care', 30.00, 237),
+('6 Foot Leash', 'Pet Supplies', 18.95, 35),
+('Tennis Balls', 'Sports', .99, 500),
+('Reeses Puffs', 'Food', 3.97, 385),
+('Daiya Chedder Cheese', 'Food', 2.99, 178),
+('Brocoli', 'Food', .79, 273),
+('Vanilla Almond Milk', 'Food', 2.89, 249);
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,31 +6,88 @@ let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Fitz",
     database: "bamazonDB",
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
-    // run start function after the connection is made to prompt user
-    start();
+    console.log("connected as id" + connection.threadId);
+    afterConnection();
+    
 });
 
+const afterConnection = () =>{
+    connection.query("SELECT * FROM products", (err, res )=>{
+        if (err) throw err;
+        console.log("Available Products:\n");
+        res.forEach(element => {
+            console.log(
+                `Department: ${element.department_name}
+                \n${element.product_name}
+                \n I.D. Number: ${element.item_id}
+                \n Units left: ${element.stock_quantity}
+                \n Price:${element.price}`
+            );
+            
+        });
+        start();
+    }
+    )};
 
-function start(){
-    inquirer 
-    .prompt({
-    name: "buy",
-    type: "rawlist",
-    message: "What would you like to buy?",
-    choices: ["buy"]
-    })
-    .then(function(answer){
-        // based on their answer
-        if (answer.buy.toUpperCase() === "BUY"){
 
+const updateProductId = function (id, quantity) {
+    let updatedStock;
+    let query = "SELECT * FROM products WHERE ?";
+    connection.query(query, { item_id: id }, function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        let productObj = res[0];
+        // console.log(productObj.stock_quantity);
+
+        if (quantity < productObj.stock_quantity) {
+            let cost;
+            updatedStock = productObj.stock_quantity - quantity;
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: updatedStock
+                    },
+                    {
+                        item_id: id
+                    }
+                ],
+                function (err) {
+                    if (err) throw err;
+                }
+            );
+            cost = quantity * productObj.price;
+            console.log(`${productObj.product_name} ordered.\nTotal Cost: $${cost}`);
         }
+        else {
+            console.log(`Insufficient quantity`);
+        }
+        connection.end();
     });
-}
+};
+
+function start() {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "id_select",
+            message: "Enter I.D. number of product",
+        },
+        {
+            type: "input",
+            name: "units",
+            message: "Enter total units to purchase"
+        }
+        ])
+        .then(function (user) {
+            updateProductId(user.id_select, user.units);
+        });
+};
 
